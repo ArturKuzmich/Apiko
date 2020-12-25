2944cd3b9512960a96e51f4d1c95aefb9598a95a
Artur <Artur1Kuzmich@gmail.com>
2020-12-25 11:09:39 +0200

    first commit

---
+++ apiko_app/src/views/Log_In.vue
@@ -0,0 +1,15 @@
+<template>
+ <div>
+   login
+ </div>
+</template>
+
+<script>
+export default {
+name: "Log_In"
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ apiko_app/src/views/New_Products.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    new Products
+  </div>
+</template>
+
+<script>
+export default {
+  name: "New_Products"
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
